5f1f8dc965f4cf0056e1e85ce1aa2fab53a7d613
ijs <isaacschrdr@gmail.com>
2021-09-03 19:13:31 -0500

    drafted GameListActivity

--- app/src/main/java/isaacjschroeder/discotron/controllers/GameListActivity.java
+++ app/src/main/java/isaacjschroeder/discotron/controllers/GameListActivity.java
@@ -9,9 +9,15 @@ import android.os.Bundle;
 import android.view.View;
 
 import io.objectbox.Box;
+import io.objectbox.query.Query;
+import io.objectbox.query.QueryBuilder;
+import isaacjschroeder.discotron.ObjectBox;
 import isaacjschroeder.discotron.R;
 import isaacjschroeder.discotron.adapters.GameRVAdapter;
+import isaacjschroeder.discotron.data.CourseModel;
+import isaacjschroeder.discotron.data.CourseModel_;
 import isaacjschroeder.discotron.data.GameModel;
+import isaacjschroeder.discotron.data.GameModel_;
 
 public class GameListActivity extends AppCompatActivity {
 
@@ -21,7 +27,7 @@ public class GameListActivity extends AppCompatActivity {
 
     private static final int SORT_AZ = 0;
     private static final int SORT_ZA = 1;
-    private static final int SORT_GP = 2;
+    private static final int SORT_DATE = 2;
     private int sortingMethod = SORT_AZ;
 
     //for determining purpose of launching this activity to behave accordingly.
@@ -54,12 +60,46 @@ public class GameListActivity extends AppCompatActivity {
                 public void onGameClick(View itemView, int position) {
                     //Launch view game details activity
                     Intent i = new Intent(GameListActivity.this, GameDetailsActivity.class);
-
+                    i.putExtra(ObjectBox.ID_EXTRA, rvAdapter.getGameID(position));
+                    startActivity(i);
                 }
             });
         }
         else {
-
+            setTitle("Select a Game");
+            rvAdapter = new GameRVAdapter(gameBox.getAll(), new GameRVAdapter.OnGameClickListener() {
+                @Override
+                public void onGameClick(View itemView, int position) {
+                    //Finish activity, returning the selected game id to be used
+                    Intent passBackId = new Intent();
+                    passBackId.putExtra(GAME_LIST_GAME_ID_EXTRA, rvAdapter.getGameID(position));
+                    setResult(RESULT_OK, passBackId);
+                    finish();
+                }
+            });
         }
+
+        rv.setAdapter(rvAdapter);
+
+        updateSortedList();
+    }
+
+    private void updateSortedList() {
+        Query<GameModel> query;
+        switch (sortingMethod) {
+            case SORT_AZ:
+                query = gameBox.query().order(GameModel_.name).build();
+                break;
+            case SORT_ZA:
+                query = gameBox.query().order(GameModel_.name, QueryBuilder.DESCENDING).build();
+                break;
+            case SORT_DATE:
+                query = gameBox.query().order(GameModel_.date).build();
+                break;
+            default:
+                query = gameBox.query().build();
+                break;
+        }
+        rvAdapter.setGamesList(query.find());
     }
 }
