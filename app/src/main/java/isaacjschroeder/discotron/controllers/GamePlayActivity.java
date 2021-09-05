5d9e2fa5cce71946d0e7a8325504aa8e6902890e
ijs <isaacschrdr@gmail.com>
2021-09-05 13:21:01 -0500

    Created finish menu item for ending game

--- app/src/main/java/isaacjschroeder/discotron/controllers/GamePlayActivity.java
+++ app/src/main/java/isaacjschroeder/discotron/controllers/GamePlayActivity.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.NumberPicker;
 
 import isaacjschroeder.discotron.R;
+import isaacjschroeder.discotron.customviews.ScoreBoardView;
 
 public class GamePlayActivity extends AppCompatActivity {
+
+    private Button enterScoreBTN;
+    private NumberPicker holeSelectNP;
+    private HorizontalScrollView hscroll;
+    private ScoreBoardView gameSBV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
+
 
 
     }
